652baa11264a932afee57797b27efe33d028b64e
hsharma961 <sharmahitesh623496@gmail.com>
2025-08-30 13:18:02 +0530

    testingCalc

---
+++ Veriable_v01/sumcalc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cout << "num1:" << num1 << "/n";
+	cout << "num2:" << num2 << "/n";
+	cin >> num1 + num2;
+
+	return 0;
+}
