12bf60e831258174612ae9f3c40e4efb87a894ab
hsharma961 <sharmahitesh623496@gmail.com>
2025-08-30 10:03:10 +0530

    veriable tested

---
+++ Veriable_v01/test.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+
+	int a = 15, b = 10, c = 5;
+
+	cout << "I have:" << a << "\nI gave RS:" << b << "\nI am left with only:";
+	cout << a - b;
+
+	return 0;
+}
--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-	std::cout << "welcome to the world man" << std::endl;
-	return 0;
-}
