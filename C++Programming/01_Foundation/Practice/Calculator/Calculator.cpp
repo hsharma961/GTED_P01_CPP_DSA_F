bb5a00785dbc27df2d97f2ff971ec4742728caf7
hsharma961 <sharmahitesh623496@gmail.com>
2025-09-15 20:00:49 +0530

    Done and Dusted

--- C++Programming/01_Foundation/Practice/Calculator/Calculator.cpp
+++ C++Programming/01_Foundation/Practice/Calculator/Calculator.cpp
@@ -1,10 +1,7 @@
-﻿// Calculator.cpp : Defines the entry point for the application.
-//
-
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello Hitesh Sharma Here.\n";
-	return 0;
+    std::cout << "Hello World!\n";
+    return 0;
 }
