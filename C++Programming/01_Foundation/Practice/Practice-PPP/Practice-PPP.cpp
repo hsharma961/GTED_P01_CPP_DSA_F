e66cc9cf91d06b9cc0ecb80cca9bbd8690829074
hsharma961 <sharmahitesh623496@gmail.com>
2025-09-17 01:30:56 +0530

    Practice Session 17 Sept

---
+++ C++Programming/01_Foundation/Practice/Practice-PPP/Practice-PPP.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main() // read name and age
+{ 
+	double age ;
+	std::string first_name = "Sharma";
+	std::string last_name = "Kumar";
+	std::cout << "Please enter your full (name & surname) and age (in months)\n";
+	std::cin >> first_name >> last_name >> age;
+	std::cout << "You know nothing " << first_name <<" " << last_name <<" (age " << age/12 << " months)\n";
+	return 0;
+}
