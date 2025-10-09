52b8002ae9f5b864029fd47399eb9d68757ac54a
hsharma961 <sharmahitesh623496@gmail.com>
2025-10-09 16:28:25 +0530

    VSCode settings modified

--- C++Programming/01_Foundation/Practice/Default_Testing.cpp
+++ C++Programming/01_Foundation/Practice/Default_Testing.cpp
@@ -5,11 +5,12 @@ int add (int x){
     x+x + (x*x);
     return x+x + (x*x);
 }
+
 int main()
 {
-    for (int i = 0; i<10; ++i)
+    for (int i = 0; i <10; ++i)
         cout << i << '\t' << add(i)<< '\n';
         
-    system("pause");
+    system ("pause");
     return 0;
 }
