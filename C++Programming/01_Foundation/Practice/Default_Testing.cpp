1c6479733a83d712809ed1fc0a3d5ab41653782c
hsharma961 <sharmahitesh623496>
2025-10-29 00:40:37 +0530

    tried vector 'BLOOP' task again

--- C++Programming/01_Foundation/Practice/Default_Testing.cpp
+++ C++Programming/01_Foundation/Practice/Default_Testing.cpp
@@ -4,33 +4,29 @@ using namespace std;
 
 int main()
 {
-    vector<string> words;
-    vector<string> disliked = {"bad"};
-    string temp;
+    vector<string> words;// word
+    vector<string> curse = {"bad"};//bad_word
 
-        cout << "enter words carefully\n";
-        while(cin >> temp) {
-            words.push_back(temp);
-        }
-            cout << "Number of words:" << words.size() << '\n';
-        
-        for (const string& word:words){
-            bool bw = false;
+    cout << "enter words carefully\n";
 
-            for (const string& is_bad:disliked){
-                if (word==is_bad){
-                bool bw = true;
-                break;
+        for(string temp; cin >> temp;){ // "temp" string for input of vector words,
+
+            bool bw=false;// bool variable to recognise "bw"(bad words), right now its kept "false" as we didn't input anything,
+            for(string bad_word:curse){// converts vector curse into string bad_word, to 
+                if(temp==bad_word){// if temp input is bad word
+                    bw=true;// bw is true
+                    }
                 }
-            }
-                                         
-        if(bw) {
-            cout << "BLOOP\n";
-        }else{ 
-            cout << word;
-        }
-        }
-    system("pause");
+                
+                    if (bw){ words.push_back("BLOOPS");}//whenever input is bad_word which is true, vector will replace "BLOOPS" with that word,
+                    else { words.push_back(temp);} 
+                }
+                    
+            for(string word:words){
+            cout<<word<<'\n';
+                }
+        cout<<"total = "<<words.size() << " words\n";
 
+    system("pause");
     return 0;
 }
