690335edb0bd1b0667285f8c10f1c868c11d9050
hsharma961 <sharmahitesh623496@gmail.com>
2025-12-12 10:46:41 -0800

    changed settings'PC RESET'

--- C++Programming/01_Foundation/Practice/Default_Testing.cpp
+++ C++Programming/01_Foundation/Practice/Default_Testing.cpp
@@ -11,7 +11,7 @@ std::vector<int> numbers = {1, 2, 3};
 
 for (int i = 0; i < numbers.size(); ++i) {
     int x = numbers[i]; // The hidden magic of the range-based loop
-    std::cout << x * 2 << " ";
+    std::cout << x * 2 << " \n";
 }
 
     system("pause");
